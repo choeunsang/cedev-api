032e31dd5ce1cd4ea998863fb755b8b600afebcd
DESKTOP-8QK34R2\genic <genicome@naver.com>
2026-05-25 11:18:35 +0900

    aaa

---
+++ src/main/java/com/cedev/api/data/dto/EstateRentItem.java
@@ -0,0 +1,42 @@
+package com.cedev.api.data.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@ToString
+public class EstateRentItem {
+
+    public String city;
+    public String bungi;
+    public String bonBun;
+    public String buBun;
+    public String dangi;
+    
+    public String gubun;
+    public String dangiB;
+    public String dediArea;
+    public String contYear;
+    public String contDate;
+    
+    public String deposit;
+    public String monPay;
+    public String floor;
+    public String consYear;
+    public String road;
+    
+    public String term;
+    public String termGubun;
+    public String rightRequ;
+    public String prevDeposit;    
+    public String prevMonPay;
+    
+    public String houseKind;    
+    private String sido;
+    private String sigungu;
+    private String dong;
+
+}
