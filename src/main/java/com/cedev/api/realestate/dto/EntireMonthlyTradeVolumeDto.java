1c82b0d55202b6c9e66a12d34fc54bde06b97db9
DESKTOP-8QK34R2\genic <genicome@naver.com>
2026-05-31 10:34:26 +0900

    a

---
+++ src/main/java/com/cedev/api/realestate/dto/EntireMonthlyTradeVolumeDto.java
@@ -0,0 +1,28 @@
+package com.cedev.api.realestate.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class EntireMonthlyTradeVolumeDto {
+
+    private String sido;
+
+    private int month01;
+    private int month02;
+    private int month03;
+    private int month04;
+    private int month05;
+    private int month06;
+    private int month07;
+    private int month08;
+    private int month09;
+    private int month10;
+    private int month11;
+    private int month12;
+
+   
+}
---
+++ src/main/java/com/cedev/api/realestate/dto/HousingTradeInfoDto.java
@@ -0,0 +1,35 @@
+package com.cedev.api.realestate.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+public class HousingTradeInfoDto {
+
+	private String city;
+	private String bungi;
+	private String bonBun;
+	private String buBun;
+	private String dangi;
+	private String dediArea;
+	private String contYear;
+	private String contDate;
+	private String amount;
+	private String floor;
+	private String consYear;
+	private String road;
+	private String dateCancel;
+	private String transType;
+	private String brokerLoc;
+	private String regiDt;
+	private String sido;
+	private String sigungu;
+	private String dong;
+    
+
+
+}
